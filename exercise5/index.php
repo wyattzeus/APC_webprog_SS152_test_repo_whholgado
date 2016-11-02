7287cc18e2b0c98fc0ad3485c2643d86111476e7
wyattzeus <whholgado@gmail.com>
2016-11-02 12:06:56 +0800

    index

---
+++ exercise5/index.php
@@ -0,0 +1,47 @@
+<?php
+include_once 'dbconfig.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<center>
+
+<div id="body">
+ <div id="content">
+    <table align="center">
+    <tr>
+    <th colspan="5"><a href="add_data.php">add data here.</a></th>
+    </tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>City Name</th>
+    <th colspan="2">Operations</th>
+    </tr>
+    <?php
+ $sql_query="SELECT * FROM users";
+ $result_set=mysql_query($sql_query);
+ while($row=mysql_fetch_row($result_set))
+ {
+  ?>
+        <tr>
+        <td><?php echo $row[1]; ?></td>
+        <td><?php echo $row[2]; ?></td>
+        <td><?php echo $row[3]; ?></td>
+  <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
+        <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
+        </tr>
+        <?php
+ }
+ ?>
+    </table>
+    </div>
+</div>
+
+</center>
+</body>
+</html>
