8c7f1a182641fd4818b1caa4cba2fa0161e7fab9
wyattzeus <whholgado@gmail.com>
2016-12-14 14:57:35 +0800

    edited

--- exercise_5/add_data.php
+++ exercise_5/add_data.php
@@ -1,68 +1,56 @@
-<!DOCTYPE HTML>  
-<html>
-<head>
-<style>
- div {
-    
-    margin-center: 350px;
-
-}
-body { 
- background-image: url("webprog bg pic.jpg");
- background-attachment: fixed;
- 
- }
- h1 {
-    color: black;
-	font-family: batang;
-	font-size: 100px;
-}
- h2 {
-    color: black;
-	font-family: batangche;
-	font-size: 30px;
-}
-.error {color: #af111c;}
-
-</style>
-</head>
-<body> 
 <?php
 include_once 'dbconfig.php';
 if(isset($_POST['btn-save']))
 {
  // variables for input data
- $name = $_POST['name'];
- $email = $_POST['email'];
- $gender = $_POST['gender'];
- $comment = $_POST['comment'];
- 
- 
- 
- 
+ $Name = $_POST['Name'];
+ $Nickname = $_POST['Nickname'];
+ $Email = $_POST['Email'];
+ $Phone_number = $_POST['Phone_number'];
+ $Home_address = $_POST['Home_address'];
+ $Gender = $_POST['Gender'];
+ $Comments = $_POST['Comments'];
  // variables for input data
- 
  // sql query for inserting data into database
- 
-        $sql_query = "INSERT INTO users(first_name,last_name,user_city) VALUES('$name','$email','$gender','$comment')";
- $con=mysqli_query($con,$sql_query);
-        
-        // sql query for inserting data into database
- 
+ $sql_query = "INSERT INTO users(Name,Nickname,Email,Home_address,Gender,Phone_number,Comments) VALUES('$Name','$Nickname','$Email','$Home_address','$Gender','$Phone_number','%Comments')";
+ // sql query for inserting data into database
+ // sql query execution function
+ if(mysqli_query($con,$sql_query))
+ {
+  ?>
+  <script type="text/javascript">
+  alert('Data Are Inserted Successfully ');
+  window.location.href='index.php';
+  </script>
+  <?php
+ }
+ else
+ {
+  ?>
+  <script type="text/javascript">
+  alert('error occurred');
+  </script>
+  <?php
+ }
+ // sql query execution function
 }
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE>
+<html>
 <head>
+<style>
+<?php include 'style.css';?>
+</style>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
+<title>My Page :)</title>
 </head>
 <body>
+<?php include 'links.php';?>
 <center>
 
 <div id="header">
- <div id="content">
-    <label>CRUD</label>
+ <div id="content2">
+    <label>Contact me</label>
     </div>
 </div>
 <div id="body">
@@ -70,22 +58,28 @@ if(isset($_POST['btn-save']))
     <form method="post">
     <table align="center">
     <tr>
-    <td align="center"><a href="wyatt.php">back to main page</a></td>
+    <td><input type="text" name="Name" placeholder="Name" required /></td>
     </tr>
     <tr>
-    <td><input type="text" name="name" placeholder="Name" required /></td>
+    <td><input type="text" name="Nickname" placeholder="Nickname" required /></td>
     </tr>
     <tr>
-    <td><input type="text" name="email" placeholder="Email" required /></td>
+    <td><input type="text" name="Email" placeholder="Email" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="Phone_number" placeholder="Phone Number" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="Home_address" placeholder="Home Address" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="Gender" placeholder="Gender" required /></td>
+    </tr>
+	<tr>
+    <td><textarea type='text' name="Comments" placeholder="Comments" rows="5" cols="40"></textarea></td>
     </tr>
     <tr>
-    <td><input type="text" name="gender" placeholder="Gender" required /></td>
-    </tr>
-    <tr>
-	<td><input type="text" name="comment" placeholder="Comment" required /></td>
-    </tr>
-    <tr>
-    <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
+    <td><button type="submit" name="btn-save" value="Submit"><strong>SAVE</strong></button></td>
     </tr>
     </table>
     </form>
@@ -94,3 +88,4 @@ if(isset($_POST['btn-save']))
 
 </center>
 </body>
+</html>
