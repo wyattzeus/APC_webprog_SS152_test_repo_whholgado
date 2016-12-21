ecef12a86aa5b8e21ed2eb47d372684dfbe54816
wyattzeus <whholgado@gmail.com>
2016-12-21 21:39:49 +0800

    fixing errors

---
+++ exercise6/CodeIgniter/application/views/links.php
@@ -0,0 +1,5 @@
+<ul>
+<li><a href="<?php echo site_url('webprog8');?>">Home</a></li>
+<li><a href="<?php echo site_url('webprog6');?>">More about me</a></li>
+<li><a href="<?php echo site_url('Register');?>">Contact me</a></li>
+</ul>
